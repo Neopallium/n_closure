312b5920c771e8f061665841615d220ffe595dbf
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2019-04-13 10:22:17 +0800

    Add example.

--- examples/closures.c
+++ examples/closures.c
@@ -1,7 +1,28 @@
 
+#include <stdio.h>
+
 #include "../n_closure.h"
 
+typedef int (*cb_func)(int x, int y, double percent, void *userdata);
+
+typedef struct {
+	int counter;
+} counter_state;
+
+int test_func(int x, int y, double percent, void *userdata) {
+	counter_state *state = (counter_state *)userdata;
+	state->counter++;
+	printf("test_func(%d, %d, %f, %p): counter=%d\n",
+			x, y, percent, userdata, state->counter);
+	return (x * percent) + (y * percent) + state->counter;
+}
+
 int main(int argc, char *argv[]) {
+	counter_state state = {counter: 0};
+	cb_func func1 = (cb_func)n_closure_new((void *)test_func);
+
+	printf("rc = %d\n", func1(100, 200, 0.10, &state));
+	printf("rc = %d\n", func1(123, 234, 0.12, &state));
 	return 0;
 }
 
